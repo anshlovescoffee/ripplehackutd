f67acaa28bbcaced91ccf868fa74152b3d476196
Jude Joubert <joubertjude@gmail.com>
2024-11-16 23:05:42 -0600

    Fix docker for api and compose

--- ml_api/main.py
+++ ml_api/main.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify, request
-from flask_core import CORS 
+from flask_cors import CORS 
 import pandas as pd
 import numpy as np
 import pickle
-
 
 with open('model/model.pkl', 'rb') as f:
     info = pickle.load(f)
